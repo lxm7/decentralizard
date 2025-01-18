15fa9da96bfd4ba337a2ea4b79f95a4add1e6342
Alex Moreton <lxmrtn7@gmail.com>
2025-01-18 16:20:40 +0700

    docker image building successfullu from scratch

--- src/app/(frontend)/maintenance/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function MaintenancePage() {
-  return (
-    <main>
-      <h1>Under Maintenance</h1>
-      <p>We’re working on it. Please check back soon!</p>
-    </main>
-  );
-}
