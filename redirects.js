19b5483b1cd2da8ce3ca46fc63ec9839af04a35f
Alex Moreton <lxmrtn7@gmail.com>
2025-01-19 16:04:34 +0700

    fix hot reload for dev

--- redirects.js
+++ redirects.js
@@ -12,7 +12,25 @@ const redirects = async () => {
     source: '/:path((?!ie-incompatible.html$).*)', // all pages except the incompatibility page
   }
 
-  const redirects = [internetExplorerRedirect]
+  const maintenance = () => {
+    if (process.env.NODE_ENV === 'development') {
+      return {
+        destination: '/maintenance.html',
+        has: [
+          {
+            type: 'header',
+            key: 'user-agent',
+            // value: '(.*Trident.*)', // all ie browsers
+          },
+        ],
+        permanent: false,
+        source: '/:path((?!maintenance.html$).*)', // all pages except the incompatibility page
+      }
+    }
+    return []
+  }
+
+  const redirects = [internetExplorerRedirect] //, maintenance()
 
   return redirects
 }
---
+++ src/app/(frontend)/maintenance/page.tsx
@@ -0,0 +1,8 @@
+export default function MaintenancePage() {
+  return (
+    <main>
+      <h1>Decentralizard is under Maintenance</h1>
+      <p>Please check back soon!</p>
+    </main>
+  );
+}
