052d8615c9e792eca694aafec617e4bd2978589b
Alex Moreton <lxmrtn7@gmail.com>
2025-01-20 15:49:00 +0700

    fix bash script post deploy

--- scripts/production.sh
+++ scripts/production.sh
@@ -3,13 +3,13 @@
 set -exo pipefail
 
 # tear down previous containers
-docker compose --file docker-compose.production.yml down -t 1
+# docker compose --file docker-compose.production.yml down -t 1
 
 # Build and run the latest version of the app
 docker compose --file docker-compose.production.yml up --build --detach nginx
 
 # reload after we have build our new container incase nginx caches anything
-sudo nginx -s reload
+# sudo nginx -s reload
 
 # Remove the unused containers
 docker system prune --force
