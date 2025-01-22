26ce0c4d42cfa2764c7f25df7664fb800ed52715
Alex Moreton <lxmrtn7@gmail.com>
2025-01-22 15:17:10 +0700

    add secure headers in next config

--- next.config.js
+++ next.config.js
@@ -9,10 +9,6 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  typescript: {
-    // remove after initial setup
-    ignoreBuildErrors: true,
-  },
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map((item) => {
@@ -26,7 +22,38 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
+  swcMinify: true,
   redirects,
+  async headers() {
+    return [
+      {
+        // Sets security headers for all routes
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self' https://decentralizard.com'; style-src 'self' ; image-src 'https://decentralizard.com';  script-src 'self' https://decentralizard.com; font-src 'self' 'https://decentralizard.com'",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+            //You can use SAMEORIGIN as a value also.
+          },
+          {
+            key: 'Permissions-Policy',
+            value:
+              "camera=(); battery=(self);  browsing-topics=(); geolocation=(); microphone=('https://abc_domain.com')",
+            //Empty brackets are used to define that we are denying them..
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default withPayload(nextConfig)
