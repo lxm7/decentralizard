59bfd676f59c3b5cdb665cde336b95510b31e98b
Alex Moreton <lxmrtn7@gmail.com>
2025-01-23 11:22:51 +0700

    include new favicon + logo svg's

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
       <head>
         <InitTheme />
         <link href="/favicon.ico" rel="icon" sizes="32x32" />
-        <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
+        <link href="/favicon.svg" rel="icon" sizes="any" type="image/svg+xml" />
       </head>
       <body>
         <Providers>
