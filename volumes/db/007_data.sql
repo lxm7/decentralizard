90bd864a5d8fbac822b1e9457e13e306ddd9e4e2
Alex Moreton <lxmrtn7@gmail.com>
2025-01-26 11:27:43 +0700

    db service ready

--- volumes/db/007_data.sql
+++ volumes/db/007_data.sql
@@ -1,65 +1,65 @@
-create table profiles (
-  id uuid references auth.users not null,
-  updated_at timestamp with time zone,
-  username text unique,
-  avatar_url text,
-  website text,
+-- create table profiles (
+--   id uuid references auth.users not null,
+--   updated_at timestamp with time zone,
+--   username text unique,
+--   avatar_url text,
+--   website text,
 
-  primary key (id),
-  unique(username),
-  constraint username_length check (char_length(username) >= 3)
-);
+--   primary key (id),
+--   unique(username),
+--   constraint username_length check (char_length(username) >= 3)
+-- );
 
-alter table profiles enable row level security;
+-- alter table profiles enable row level security;
 
-create policy "Public profiles are viewable by the owner."
-  on profiles for select
-  using ( auth.uid() = id );
+-- create policy "Public profiles are viewable by the owner."
+--   on profiles for select
+--   using ( auth.uid() = id );
 
-create policy "Users can insert their own profile."
-  on profiles for insert
-  with check ( auth.uid() = id );
+-- create policy "Users can insert their own profile."
+--   on profiles for insert
+--   with check ( auth.uid() = id );
 
-create policy "Users can update own profile."
-  on profiles for update
-  using ( auth.uid() = id );
+-- create policy "Users can update own profile."
+--   on profiles for update
+--   using ( auth.uid() = id );
 
--- Set up Realtime
-begin;
-  drop publication if exists supabase_realtime;
-  create publication supabase_realtime;
-commit;
-alter publication supabase_realtime add table profiles;
+-- -- Set up Realtime
+-- begin;
+--   drop publication if exists supabase_realtime;
+--   create publication supabase_realtime;
+-- commit;
+-- alter publication supabase_realtime add table profiles;
 
--- Set up Storage
-insert into storage.buckets (id, name)
-values ('avatars', 'avatars');
+-- -- Set up Storage
+-- insert into storage.buckets (id, name)
+-- values ('avatars', 'avatars');
 
-create policy "Avatar images are publicly accessible."
-  on storage.objects for select
-  using ( bucket_id = 'avatars' );
+-- create policy "Avatar images are publicly accessible."
+--   on storage.objects for select
+--   using ( bucket_id = 'avatars' );
 
-create policy "Anyone can upload an avatar."
-  on storage.objects for insert
-  with check ( bucket_id = 'avatars' );
+-- create policy "Anyone can upload an avatar."
+--   on storage.objects for insert
+--   with check ( bucket_id = 'avatars' );
 
-create policy "Anyone can update an avatar."
-  on storage.objects for update
-  with check ( bucket_id = 'avatars' );
+-- create policy "Anyone can update an avatar."
+--   on storage.objects for update
+--   with check ( bucket_id = 'avatars' );
 
 
 
 
 -- Create default storage bucket
--- INSERT INTO storage.buckets (id, name, public)
--- VALUES ('avatars', 'avatars', true)
--- ON CONFLICT (id) DO NOTHING;
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('avatars', 'avatars', true)
+ON CONFLICT (id) DO NOTHING;
 
--- -- Create initial Payload admin user
--- INSERT INTO payload.users (email, password, roles)
--- VALUES (
---   'admin@example.com', 
---   crypt('adminpassword', gen_salt('bf')), 
---   '{"admin"}'
--- )
--- ON CONFLICT (email) DO NOTHING;
+-- Create initial Payload admin user
+INSERT INTO payload.users (email, password, roles)
+VALUES (
+  'lxmrtn7@gmail.com', 
+  crypt('adminpassword', gen_salt('bf')), 
+  '{"admin"}'
+)
+ON CONFLICT (email) DO NOTHING;
